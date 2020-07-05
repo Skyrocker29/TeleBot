334e450521eef539e2dbf82ddf46c33ae98fc283
Skyrocker29 <67867377+Skyrocker29@users.noreply.github.com>
2020-07-05 19:20:58 +0300

    Заработал гит

--- bot.py
+++ bot.py
@@ -23,7 +23,7 @@ def send_text(message):
 	if message.text.lower() == 'привет':
 		bot.send_message(message.chat.id, 'Привет, ')
 	elif message.text.lower() == 'пока':
-		bot.send_message(message.chat.id, 'Прощай, Санек.')
+		bot.send_message(message.chat.id, 'Прощай, Санек..')
 	elif message.text.lower() == 'люблю':
 		bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAMoXujAVuiydTSBFOD2suwkrPm49NUAAkgDAALGzGMCF5HWtlHzZjIaBA')
 	else:
