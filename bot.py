36a2ce1b6ccdae1c408de37adafc435eea237f1a
Skyrocker29 <67867377+Skyrocker29@users.noreply.github.com>
2020-07-05 23:27:37 +0300

    Исправление ЩЦ

--- bot.py
+++ bot.py
@@ -4,15 +4,15 @@ import apiai, json
 
 bot = telebot.TeleBot(config.token)
 keyboard1 = telebot.types.ReplyKeyboardMarkup(True)
-keyboard1.row('Привет', 'Работаем', 'Как?')
+keyboard1.row('Привет', 'Работаем', 'Деньги нужны', 'До свидания')
 
 @bot.message_handler(commands = ['start'])
 def start_message(message):
 	bot.send_message(message.chat.id, 'Слушай меня, слушай!', reply_markup = keyboard1)
 
-@bot.message_handler(commands = ['Привет'])
+@bot.message_handler(commands = ['Хайль Гидра'])
 def start_message(message):
-	bot.send_message(message.chat.id, 'Как обстановка?', reply_markup = keyboard1)
+	bot.send_message(message.chat.id, 'Привет, ты написал мне /start', reply_markup = keyboard1)
 
 @bot.message_handler(content_types = ['text'])
 def send_text(message):
@@ -21,11 +21,13 @@ def send_text(message):
 
 	request.session_id = 'TestBot'
 	if message.text.lower() == 'привет':
-		bot.send_message(message.chat.id, 'Привет, друг!!!')
-	elif message.text.lower() == 'пока':
-		bot.send_message(message.chat.id, 'Прощай, Санек..')
-	elif message.text.lower() == 'люблю':
-		bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAMoXujAVuiydTSBFOD2suwkrPm49NUAAkgDAALGzGMCF5HWtlHzZjIaBA')
+		bot.send_message(message.chat.id, 'Как обстановка? Всё в порядке?')
+	elif message.text.lower() == 'работаем':
+		bot.send_message(message.chat.id, 'Помощь нужна? Часа хватит?')
+	elif message.text.lower() == 'деньги нужны':
+		bot.send_sticker(message.chat.id, 'Как? Послушай меня, послушай! Деньги это грязь!')
+	elif message.text.lower() == 'до свидания':
+		bot.send_sticker(message.chat.id, 'До завтра, мужики!')
 	else:
 		request.query = message.text
 		responseJson = json.loads(request.getresponse().read().decode('utf-8'))
@@ -33,7 +35,7 @@ def send_text(message):
 		if response:
 			bot.send_message(message.chat.id, response)
 		else:
-			bot.send_message(message.chat.id, 'Я вас не понял')
+			bot.send_message(message.chat.id, 'Ну что ты мне даешь епона мать?!')
 
 @bot.message_handler(content_types = ['sticker'])
 def send_id_sticker(message):
